1178a904a8cee69276d37d6e94c06fc10c97f58a
Gulshan971 <gulshanrajdev47595@gmail.com>
2026-05-02 15:39:51 +0530

    Modified App by adding React fragment functionality

--- Dekisuzi/src/App.jsx
+++ Dekisuzi/src/App.jsx
@@ -1,12 +1,14 @@
+import React ,{Fragment} from "react" ;
 export const App = () => {
 return(
-<div>
+<React.Fragment>
     <h1>Glad to Welcome you on my Website</h1>
     <h2>Myself Gulshan Rajdev</h2>
     <p>
         A third year Btech Student . 
         Interseting person desire to achieve everything. 
     </p>
-</div>
-)
+</React.Fragment>
+);
 }
+console.log(React.createElement('h3' , null , 'Welcome to BBAU')) ;
