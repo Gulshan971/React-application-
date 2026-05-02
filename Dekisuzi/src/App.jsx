ab24e39ed519e54d4b9a78271a79d1259f9653d7
Gulshan971 <gulshanrajdev47595@gmail.com>
2026-05-02 11:33:23 +0530

    My first commit for react project

---
+++ Dekisuzi/src/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+return(
+<div>
+    <h1>Glad to Welcome you on my Website</h1>
+    <h2>Myself Gulshan Rajdev</h2>
+    <p>
+        A third year Btech Student . 
+        Interseting person desire to achieve everything. 
+    </p>
+</div>
+)
+}
