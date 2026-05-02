645bcbdc3fc1ab608a8f8fdc77131e8dc2b08e02
Gulshan971 <gulshanrajdev47595@gmail.com>
2026-05-02 12:08:54 +0530

    Made my Resume

---
+++ Dekisuzi/src/Resume.jsx
@@ -0,0 +1,13 @@
+export const Resume = () => {
+    return (
+        <div>
+            <h3>Mobile Number : <p>7985476928</p></h3>
+            <h3>Email : <p>gulshanrajdev47595@gmial.com</p> </h3>
+            <h3>About : Hello myself Gulshan Rajdev. I believe learning is the tool that can make any situation true. This ideology inspire me to extract minute information about every aspect . I am interested in Game Development , Web development and AI-ML . I am third year B-Tech student at BBAU , Central University , Lucknow . I wishes to make time travel possible via using upgrading technology . Pronoun : He / him / his Fun Fact : I do learn from my failure and I don't know how to click on a mouse in my school . 
+            </h3>
+            <div >
+            <img src="https://th.bing.com/th/id/OIP.qERRGBFe0kwb_5Rq6eKClgHaE7?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3" alt="GR" width = "40%" height = "40%"/>
+            </div>
+        </div>
+    )
+}
